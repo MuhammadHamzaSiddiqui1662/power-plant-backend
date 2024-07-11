f99e8d3e1ed91b533d2eaabf08ffa8fc2980e26a
Innova8ors12 <developer@innova8ors.com>
2024-07-11 19:14:58 +0500

    fix: user schema updated, firstname and lastname property merged into name property

--- src/features/user/user.entity.ts
+++ src/features/user/user.entity.ts
@@ -1,68 +1,67 @@
 import { Schema, model } from "mongoose";
 
-const userSchema = new Schema({
-    firstName: {
-        type: String,
-        required: [true, "First name is a required field"],
-    },
-    lastName: {
-        type: String,
-        required: [true, "Last name is a required field"],
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is a required field"],
     },
     email: {
-        type: String,
-        required: [true, "Email is a required field"],
-        unique: true,
+      type: String,
+      required: [true, "Email is a required field"],
+      unique: true,
     },
     password: {
-        type: String,
-        required: [true, "Password is a required field"],
+      type: String,
+      required: [true, "Password is a required field"],
     },
     phone: {
-        type: String,
-        required: [true, "Phone number is a required field"],
+      type: String,
+      required: [true, "Phone number is a required field"],
     },
     type: {
-        type: String,
-        required: [true, "Type is a required field"],
+      type: String,
+      required: [true, "Type is a required field"],
     },
     birthDate: {
-        type: Date,
-        required: [true, "Birth date is a required field"],
+      type: Date,
+      required: [true, "Birth date is a required field"],
     },
     imageUrl: {
-        type: String,
-        default: "/placeholder-avatar.png"
+      type: String,
+      default: "/placeholder-avatar.png",
     },
     location: {
-        type: String,
+      type: String,
     },
     earning: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
     listing: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
     status: {
-        type: String,
-        required: [true, "Status is a required field"],
+      type: String,
+      required: [true, "Status is a required field"],
     },
     otp: {
-        type: String,
+      type: String,
     },
     otpExpiry: {
-        type: Date,
+      type: Date,
     },
     online: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     lastSeen: {
-        type: Date,
-        default: Date.now
+      type: Date,
+      default: Date.now,
     },
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
 export const User = model("User", userSchema);
